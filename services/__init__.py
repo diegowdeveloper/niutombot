d24f16155a9da181da4deedace7f9aca40468844
Diego Castro <castrogarcia2017@gmail.com>
2025-10-28 17:56:32 -0400

    Niutom V1.2.5

--- services/__init__.py
+++ services/__init__.py
@@ -1,9 +1,9 @@
-from messageHandler import MessageHandler
-from whatsAppService import WhatsAppService
-from geminiService import GeminiService
-from langChainService import LangChainGemini
-from azureNiutomCompendium import AzureNiutomCompendium
-from tavilySearch import TavilySearch
+from .messageHandler import MessageHandler
+from .whatsAppService import WhatsAppService
+from .geminiService import GeminiService
+from .langChainService import LangChainGemini
+from .azureNiutomCompendium import AzureNiutomCompendium
+from .tavilySearch import TavilySearch
 
 __all__ = [
     "WhatsAppService",
